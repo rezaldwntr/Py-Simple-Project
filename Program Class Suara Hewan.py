a676597434e1057ae620b7c1c9a0eb64e96dba3c
Husna Rezal Dewantara <50539422+rezaldwntr@users.noreply.github.com>
2025-06-17 10:24:43 +0800

    Add files via upload

---
+++ Program Class Suara Hewan.py
@@ -0,0 +1,71 @@
+#Base Class
+class Animal:
+  def make_sound(self):
+    print("Some generic animal sound")
+
+# Derived Classes
+class Dog(Animal):
+  def make_sound(self):
+    print("Woof! Woof!")
+
+class Cat(Animal):
+  def make_sound(self):
+    print("Meow! Meow!")
+
+class Cow(Animal):
+  def make_sound(self):
+    print("Moo! Moo!")
+
+class Duck(Animal):
+  def make_sound(self):
+    print("Quack! Quack!")
+
+# Simulator Class
+class AnimalSoundSimulator:
+  def __init__(self):
+    self.animals = []
+
+  def add_animal(self, animal):
+    if isinstance(animal, Animal):
+      self.animals.append(animal)
+      print(f"{animal.__class__.__name__} added to the simulator")
+    else:
+      print("Invalid animal type")
+
+  def make_all_sounds(self):
+    if not self.animals:
+      print("No animals in the simulator")
+    else:
+      print("\n--- Animal Sounds ---")
+      for animal in self.animals:
+        animal.make_sound()
+
+#Main Program
+simulator = AnimalSoundSimulator()
+
+while True:
+  print("\n--- Animal Sound Simulator ---")
+  print("1. Add Dog")
+  print("2. Add Cat")
+  print("3. Add Cow")
+  print("4. Add Duck")
+  print("5. Make All Sounds")
+  print("6. Exit")
+
+  choice = input("Enter your choice (1-6): ")
+
+  if choice == '1':
+    simulator.add_animal(Dog())
+  elif choice == '2':
+    simulator.add_animal(Cat())
+  elif choice == '3':
+    simulator.add_animal(Cow())
+  elif choice == '4':
+    simulator.add_animal(Duck())
+  elif choice == '5':
+    simulator.make_all_sounds()
+  elif choice == '6':
+    print("Exiting the simulator. Goodbye!")
+    break
+  else:
+    print("Invalid choice. Please try again.")
